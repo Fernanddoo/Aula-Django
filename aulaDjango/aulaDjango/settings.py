e873b49255390bb5767895ce0df079bc0468df61
Fernanddoo <awpfernandodradunz@gmail.com>
2025-09-01 19:31:27 -0300

    secret key na .env

--- aulaDjango/aulaDjango/settings.py
+++ aulaDjango/aulaDjango/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4c(wx%97e@vqw_8e9txaxn7c6y$9hn(y=1&%b5nm*$_xag49j+'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
